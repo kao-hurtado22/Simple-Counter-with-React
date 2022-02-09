6b7218963efc9730b4daaa66c655bea38aae8725
kao-hurtado22 <kao.hurtado22@gmail.com>
2022-02-09 15:05:45 +0000

    terminando simple counter

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,48 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import propTypes from "prop-types";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+function simpleCounter(props) {
+	return (
+		<div className="bigCounter">
+			<div className="calendar">
+				<i className="far fa-clock"></i>
+			</div>
+			<div className="four">{props.digitFour % 10}</div>
+			<div className="three">{props.digitThree % 10}</div>
+			<div className="two">{props.digitTwo % 10}</div>
+			<div className="one">{props.digitOne % 10}</div>
+		</div>
+	);
+}
+simpleCounter.propTypes = {
+	digitFour: propTypes.number,
+	digitThree: propTypes.number,
+	digitTwo: propTypes.number,
+	digitOne: propTypes.number,
+};
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let Counter = 0;
+setInterval(function () {
+	const four = Math.floor(Counter / 1000);
+	const three = Math.floor(Counter / 100);
+	const two = Math.floor(Counter / 10);
+	const one = Math.floor(Counter / 1);
+
+	console.log(four, three, two, one);
+	Counter++;
+
+	ReactDOM.render(
+		<simpleCounter
+			digiOne={one}
+			digiTwo={two}
+			digiThree={three}
+			digiFour={four}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
